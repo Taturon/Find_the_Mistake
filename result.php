a623c42645dde29f9f51e21a5b01de090d3939e1
Taturon <taturon5511@gmail.com>
2020-07-25 13:42:27 +0900

    [Add] DBに結果が保存されるようになりました。

--- result.php
+++ result.php
@@ -11,13 +11,23 @@ if (empty($_SESSION) || empty($_POST)) {
 //回答時間を算出
 $end_time = microtime(true);
 $start_time = $_SESSION['start_time'];
-$time = round($end_time - $start_time, 2);
+$time = round($end_time - $start_time, 2) . '秒';
+
+// 回答時間が100秒以上の場合は値を上書き
+if ($time > 100) {
+	$time = '100秒以上';
+}
 
 // セッション変数を変数に格納
 $name = $_SESSION['name'];
 $difficulty = $_SESSION['difficulty'];
 $correct = $_SESSION['correct'];
-$count = $_SESSION['count'];
+$count = $_SESSION['count'] . '回';
+
+// リセット回数が100回以上の場合は値を上書き
+if ($count > 100) {
+	$count = '100回以上';
+}
 
 // セッション変数の初期化
 $_SESSION = [];
@@ -35,6 +45,13 @@ if ($correct === $answer) {
 	$result = '不正解です。。。';
 }
 
+// db接続
+require_once('db_connect.php');
+
+// 新規登録処理
+$sql = 'INSERT INTO find_the_mistake (name, difficulty, time, reset) VALUES (?, ?, ?, ?)';
+$stmt = $db->prepare($sql);
+$stmt->execute([$name, $difficulty, $time, $count]);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -44,8 +61,8 @@ if ($correct === $answer) {
 </head>
 <body>
 <h1>結果は...<?php echo $result ?></h1>
-<h2>回答時間: <?php echo $time; ?>秒</h2>
-<h2>リセット回数: <?php echo $count; ?>回</h2>
+<h2>回答時間: <?php echo $time; ?></h2>
+<h2>リセット回数: <?php echo $count; ?></h2>
 <a href="start.php"><button type="button">スタートページへ</button></a>
 </body>
 </html>
