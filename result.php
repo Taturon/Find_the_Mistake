e8538ca18e313a6ff2f97d25eec9ad09594c64d8
Taturon <taturon5511@gmail.com>
2020-07-25 14:48:47 +0900

    [Fix] 不正解でもランキングに登録されてしまう不具合を修正しました

--- result.php
+++ result.php
@@ -11,7 +11,7 @@ if (empty($_SESSION) || empty($_POST)) {
 //回答時間を算出
 $end_time = microtime(true);
 $start_time = $_SESSION['start_time'];
-$time = round($end_time - $start_time, 2) . '秒';
+$time = sprintf('%05.2f', round($end_time - $start_time, 2)) . '秒';
 
 // 回答時間が100秒以上の場合は値を上書き
 if ($time > 100) {
@@ -22,7 +22,7 @@ if ($time > 100) {
 $name = $_SESSION['name'];
 $difficulty = $_SESSION['difficulty'];
 $correct = $_SESSION['correct'];
-$count = $_SESSION['count'] . '回';
+$count = sprintf('%02d', $_SESSION['count']) . '回';
 
 // リセット回数が100回以上の場合は値を上書き
 if ($count > 100) {
@@ -39,19 +39,25 @@ session_destroy();
 // 回答を変数に格納
 $answer = html_entity_decode($_POST['answer']);
 
+// 正解・不正解によるメッセージの分岐
 if ($correct === $answer) {
 	$result = '正解です！';
+	$flg = 1;
 } else {
 	$result = '不正解です。。。';
+	$flg = 0;
 }
 
-// db接続
-require_once('db_connect.php');
+// 正解時のみDBに登録
+if ($flg === 1) {
+	// db接続
+	require_once('db_connect.php');
 
-// 新規登録処理
-$sql = 'INSERT INTO find_the_mistake (name, difficulty, time, reset) VALUES (?, ?, ?, ?)';
-$stmt = $db->prepare($sql);
-$stmt->execute([$name, $difficulty, $time, $count]);
+	// 新規登録処理
+	$sql = 'INSERT INTO find_the_mistake (name, difficulty, time, reset) VALUES (?, ?, ?, ?)';
+	$stmt = $db->prepare($sql);
+	$stmt->execute([$name, $difficulty, $time, $count]);
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
