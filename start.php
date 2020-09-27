399b8571078e4879ac3e7c92a8a6f8dd5de6b090
Taturon <taturon5511@gmail.com>
2020-09-27 22:26:25 +0900

    [Modify] phpタグの短縮形を用いてコードを整理

--- start.php
+++ start.php
@@ -28,7 +28,7 @@ if (isset($_POST['name'], $_POST['difficulty'])) {
 	}
 }
 
-// セッション変数の初期化
+// セッションの放棄
 $_SESSION = [];
 setcookie(session_name(), '', time() - 1, '/');
 session_destroy();
@@ -45,21 +45,26 @@ session_destroy();
 <p>20×20個のボタンが表示されるので、指定されたボタンを見つけ出して押しましょう！</p>
 <?php if (isset($error_msg)): ?>
 <hr><ul>
-<li><?php echo $error_msg; ?></li>
+<li><?= $error_msg; ?></li>
 </ul><hr>
 <?php endif; ?>
 <form method="POST">
-<label>名前<small> (10字以内)</small><br><input type="text" name="name" required value="<?php
-if (isset($name)) echo $name;
-?>"></label>
+<label>名前
+<small> (10字以内)</small><br>
+<input type="text" name="name" required value="<?php if (isset($name)) echo $name;?>">
+</label>
 <p>
 <span>難易度</span><br>
-<label><input type="radio" name="difficulty" value="易しい（絵文字）" <?php
-if (empty($difficulty) || isset($difficulty) && $difficulty === '易しい（絵文字）') echo 'checked';
-?>>易しい（絵文字）</label>
-<label><input type="radio" name="difficulty" value="難しい（漢字）" <?php
-if (isset($difficulty) && $difficulty === '難しい（漢字）') echo 'checked';
-?>>難しい（漢字）</label>
+<label>
+<input type="radio" name="difficulty" value="易しい（絵文字）"
+<?php if (empty($difficulty) || isset($difficulty) && $difficulty === '易しい（絵文字）') echo 'checked';?>
+>易しい（絵文字）
+</label>
+<label>
+<input type="radio" name="difficulty" value="難しい（漢字）"
+<?php if (isset($difficulty) && $difficulty === '難しい（漢字）') echo 'checked';?>
+>難しい（漢字）
+</label>
 </p>
 <input type="submit" value="問題に挑戦!（時間計測が開始されます）">
 </form>
